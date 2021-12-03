99ae30e10f6cd8a1ffbf4d0d2c87c9b0481fe38c
Casper Thomsen <casperpvp@gmail.com>
2021-12-03 12:33:32 +0100

    Updated to correct usage of Bootstrap rows

--- public_www/page2.php
+++ public_www/page2.php
@@ -4,12 +4,14 @@
   <body>
     <?php include("navbar.php"); ?>
     <div class="container">
-      <div class="bg-light p-5 rounded mt-3">
-        <center>
-          <img src="assets/img/logo.png" alt="GeekHosting logo">
-          <h1>This new webserver is hosted by GeekHosting!</h1>
-          <p>This is page 2.</p>
-        </center>
+      <div class="row">
+        <div class="bg-light p-5 rounded mt-3">
+          <center>
+            <img src="assets/img/logo.png" alt="GeekHosting logo">
+            <h1>This new webserver is hosted by GeekHosting!</h1>
+            <p>This is page 2.</p>
+          </center>
+        </div>
       </div>
     </div>
     <?php include("footer.php"); ?>
